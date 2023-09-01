7274c9b3ae349e453cfac08f33751adcce778fa5
Angel Amélia Halouane <63232950+angeleads@users.noreply.github.com>
2023-09-01 18:53:05 +0200

    [ADD] About us is done

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,84 +1,29 @@
 export const metadata = {
     title: 'SharetoGo - About',
-    description: 'About SharetoGo',
-  };
-  
-  export default function Contact() {
-    return (
-      <section className="bg-green-100 py-16">
-        <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="text-center">
-                <h2 className="text-4xl font-extrabold text-gray-800 mb-4">Ayuda</h2>
-                <p className="text-gray-600 mb-2 font-bold text-2xl">Ponte en contacto con nosotros</p>
-                <p className="text-gray-600 mb-8 text-lg">Contacta con nosotros para cualquier duda o consulta y te ayudaremos lo antes posible.</p>
-            </div>
+    description: 'AboutSharetoGo',
+};
 
-          <form className="bg-transparent rounded-lg shadow-2xl p-6 grid grid-cols-2 gap-4">
-            <div className="col-span-1">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
-                Nombre
-              </label>
-              <input
-                className="w-full px-3 py-2 rounded-lg"
-                type="text"
-                placeholder="Tu nombre"
-                id="nombre"
-                name="nombre"
-                required
-              />
+import Image from 'next/image'
+import AboutPic from '@/public/images/about.png'
+   
+export default function About() {
+  return (
+        <section className="bg-green-100 py-16">
+          <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="text-center mt-10">
+              <h2 className="text-4xl font-extrabold text-gray-800 mb-4">Quiénes somos?</h2>
+              <p className="text-gray-800 mb-2 font-bold text-2xl">Nosotros</p>
+              <p className="text-gray-800 mb-8 text-lg">
+              Compartir nos mueve, por ello, en SharetoGo nos hemos propuesto ser la solución lógica y la herramienta real para llevar a cabo el carpooling.
+              </p>
+              <p className="text-gray-800 mb-8 text-lg">Somos personas que trabajamos ambiciosamente día tras día para poder conseguir un cambio de hábitos en la movilidad de la ciudadanía. Entre todos, lo conseguiremos.</p>
+              <p className="text-gray-800 mb-8 text-lg font-bold italic">
+              Para ti, para todos, comparte.
+              </p>
             </div>
-            <div className="col-span-1">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="apellido">
-                Apellido/s
-              </label>
-              <input
-                className="w-full px-3 py-2 border rounded-lg "
-                type="text"
-                placeholder="Tu apellido"
-                id="apellido"
-                name="apellido"
-                required
-              />
-            </div>
-  
-            <div className="col-span-2">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                Correo electrónico (obligatorio)
-              </label>
-              <input
-                className="w-full px-3 py-2 border rounded-lg "
-                type="email"
-                placeholder="Tu correo electrónico"
-                id="email"
-                name="email"
-                required
-              />
-            </div>
-  
-            <div className="col-span-2">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="mensaje">
-                Mensaje
-              </label>
-              <textarea
-                className="w-full px-3 py-2 border rounded-lg "
-                id="mensaje"
-                placeholder="Tu mensaje..."
-                name="mensaje"
-                required
-              ></textarea>
-            </div>
-  
-            <div className="col-span-2 text-center">
-              <button
-                className="bg-green-500 text-white py-2 px-4 rounded-lg"
-                type="submit"
-              >
-                Enviar mensaje
-              </button>
-            </div>
-          </form>
-        </div>
-      </section>
-    );
-  }
-  
+            <Image src={ AboutPic } alt="about picture"/>
+    
+          </div>
+        </section>    
+  );
+}
--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -36,7 +36,7 @@ export default function Header() {
             {/* Desktop sign in links */}
             <ul className="flex grow justify-end flex-wrap items-center">
               <li>
-                <Link href="/" className="font-medium text-gray-600 hover:underline hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Quiénes somos?</Link>
+                <Link href="/about" className="font-medium text-gray-600 hover:underline hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Quiénes somos?</Link>
               </li>
               <li>
                 <Link href="/contact" className="font-medium text-gray-600 hover:underline hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Contacto</Link>
