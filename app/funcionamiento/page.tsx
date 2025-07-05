6b5ead0f32c69fcb5df2f7c8a536081211f72d22
Nacho De Udaeta <naxo@Nachos-MacBook-Air.local>
2025-07-05 19:38:40 +0200

    archivos creados para cada página

---
+++ app/funcionamiento/page.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'SharetoGo - Idioma',
+  description: 'Idioma page of SharetoGo',
+}
+
+import Header from '@/components/ui/header'
+
+export default function Funcionamiento() {
+  return (
+    <div className="bg-white">
+      <Header />
+    </div>
+  )
+}
+
+//cambiar, esto no es una página
---
+++ app/idioma/page.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'SharetoGo - Idioma',
+  description: 'Idioma page of SharetoGo',
+}
+
+import Header from '@/components/ui/header'
+
+export default function Idioma() {
+  return (
+    <div className="bg-white">
+      <Header />
+    </div>
+  )
+}
+
+//cambiar, esto no es una página
