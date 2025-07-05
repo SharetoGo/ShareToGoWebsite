fcb0bd86f003de16f686ccb2417ab2984852bc07
Nacho De Udaeta <naxo@Nachos-MacBook-Air.local>
2025-07-05 19:38:02 +0200

    rutas añadidas en el header

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -5,24 +5,18 @@ import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import Logo from './logo'
 import MobileMenu from './mobile-menu'
+import { MdLanguage } from "react-icons/md"
+
 
 export default function Header() {
 
-  const [top, setTop] = useState<boolean>(true)
-
-  // detect whether user has scrolled the page down by 10px
-  const scrollHandler = () => {
-    window.pageYOffset > 10 ? setTop(false) : setTop(true)
-  }  
-
-  useEffect(() => {
-    scrollHandler()
-    window.addEventListener('scroll', scrollHandler)
-    return () => window.removeEventListener('scroll', scrollHandler)
-  }, [top])
-
   return (
-    <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top ? 'bg-green-50 backdrop-blur-sm shadow-sm' : ''}`}>
+<header
+  className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${
+    'backdrop-blur-sm shadow-sm' 
+  }`}
+  style={{ backgroundColor: '#9dd187' } }
+>
       <div className="max-w-8xl mx-auto px-5 sm:px-6">
         <div className="flex items-center justify-between h-16 md:h-20">
 
@@ -42,10 +36,25 @@ export default function Header() {
                 <Link href="/about" className="font-medium text-teal-950 hover:underline hover:text-teal-700 px-5 py-3 flex items-center transition duration-150 ease-in-out">Quiénes somos</Link>
               </li>
               <li>
+                <Link href="/contratar" className="font-medium text-teal-950 hover:underline hover:text-teal-700 px-5 py-3 flex items-center transition duration-150 ease-in-out">Contratar</Link> 
+              </li>
+              <li>
+                <Link href="/espacioEmpresas" className="font-medium text-teal-950 hover:underline hover:text-teal-700 px-5 py-3 flex items-center transition duration-150 ease-in-out">Espacio Empresas</Link> 
+              </li>
+              <li>
+                <Link href="/funcionamiento" className="font-medium text-teal-950 hover:underline hover:text-teal-700 px-5 py-3 flex items-center transition duration-150 ease-in-out">Funcionamiento de la App</Link> 
+              </li>
+              <li>
                 <Link href="/contact" className="font-medium text-teal-950 hover:underline hover:text-teal-700 px-5 py-3 flex items-center transition duration-150 ease-in-out">Contacto</Link>
               </li>
               <li>
                 <Link href="/privacy" className="font-medium text-teal-950 hover:underline hover:text-teal-700 px-5 py-3 flex items-center transition duration-150 ease-in-out">Política de privacidad</Link>
+              </li>
+              <li>
+                <Link href="/idioma" className="font-medium text-teal-950 hover:underline hover:text-teal-700 px-5 py-3 flex items-center transition duration-150 ease-in-out">
+                Idioma 
+                <MdLanguage className="text-xl text-teal-950" />
+                </Link> {/* CAMBIAR */}
               </li>
             </ul>
 
