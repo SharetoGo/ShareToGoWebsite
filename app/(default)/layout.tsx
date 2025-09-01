050a20e92620419faaccd26a8b0d46aed8876c12
cyprien <cyprien@cypriens-macbook-pro-1.home>
2025-09-01 17:19:30 +0200

    [ADD] adding the Carpooling para todos as the description of the sharetogo

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect } from 'react'
+import Head from 'next/head';
 
 import AOS from 'aos'
 import 'aos/dist/aos.css'
@@ -24,6 +25,9 @@ export default function DefaultLayout({
 
   return (
     <>
+    <Head>
+        <title>SharetoGo - Carpooling para todos</title>
+      </Head>
       <main className="grow">
 
         {children}
