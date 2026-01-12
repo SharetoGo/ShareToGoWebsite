a3f3f03fa5474e4bab3d8852fb21a0e46a586cd4
angeleads <halouane.angel@epitech.eu>
2026-01-12 12:15:19 +0100

    [ADD] favicon added need to be checked

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,11 +8,25 @@ import Footer from "@/components/footer";
 import { Suspense } from "react";
 import ClientI18nProvider from "@/components/ClientI18nProvider";
 
-export const metadata: Metadata = { /* ... */ };
+export const metadata: Metadata = {
+  title: "ShareToGo - Plataforma de coche compartido para empresas",
+  description:
+    "Optimiza la movilidad corporativa y fomenta la sostenibilidad con ShareToGo, la plataforma de coche compartido diseñada para empresas.",
+  keywords: [
+    "ShareToGo",
+    "coche compartido",
+    "movilidad corporativa",
+    "sostenibilidad empresarial",
+    "plataforma de coche compartido",
+    "optimización de transporte",
+    "empleados",
+  ],
+ };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         <Suspense fallback={<div>Loading...</div>}>
           <ClientI18nProvider>
