ec4f599ab5951abd519c0957687d916470512a94
angeleads <halouane.angel@epitech.eu>
2026-02-07 16:30:41 +0100

    [FIX] still trying to fix the translation

--- components/ClientI18nProvider.tsx
+++ components/ClientI18nProvider.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import React from 'react';
 import { I18nextProvider } from 'react-i18next';
 import i18n from '@/lib/i18n';
 
--- lib/i18n.ts
+++ lib/i18n.ts
@@ -1,33 +1,24 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 import en from '@/public/locales/en/common.json';
 import es from '@/public/locales/es/common.json';
 import fr from '@/public/locales/fr/common.json';
 
-const isServer = typeof window === 'undefined';
-
-  i18n
-    .use(initReactI18next)
-    .use(LanguageDetector)
-    .init({
-      resources: {
-        en: { translation: en },
-        es: { translation: es },
-        fr: { translation: fr },
-      },
-      fallbackLng: 'es',
-      lng: 'es',
-      interpolation: {
-        escapeValue: false,
-      },
-      detection: {
-        order: ['localStorage', 'navigator', 'htmlTag'],
-        caches: ['localStorage'],
-      },
-      supportedLngs: ['es', 'en', 'fr'],
-    });
-
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      es: { translation: es },
+      fr: { translation: fr },
+    },
+    lng: 'es',
+    fallbackLng: 'es',
+    supportedLngs: ['es', 'en', 'fr'],
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
 export default i18n;
